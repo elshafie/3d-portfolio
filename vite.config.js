dcd846a5c7784a9fdc18d7ce1456d758b8d6feb0
elshafie <m_elshafie15@yahoo.com>
2024-02-12 03:04:07 +0200

    edit json and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3d-portfolio",
+  base: "/3d-portfolio/",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
   optimizeDeps: {
