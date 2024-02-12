f892766ad699c8f74898ac397b2e3d32ffed22b0
Mohammed Elshafie <67983759+elshafie@users.noreply.github.com>
2024-02-12 03:26:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3d-portfolio/",
+  base: "https://elshafie.github.io/3d-portfolio/",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
   optimizeDeps: {
